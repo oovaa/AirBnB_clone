a19508509445bd0bd55c6591a17cbe7092c004e3
Isra Hatim <138698238+EsraaGD@users.noreply.github.com>
2024-01-10 22:04:41 +0300

    Create command task 7

--- console.py
+++ console.py
@@ -2,6 +2,8 @@
 """CLI"""
 
 import cmd
+from models.base_model import BaseModel
+from models import storage
 
 
 class HBNBCommand(cmd.Cmd):
@@ -20,6 +22,23 @@ class HBNBCommand(cmd.Cmd):
     def emptyline(self):
         """Execute nothing"""
         pass
+
+    def do_create(self, *args):
+        """Create instance of BaseModel & save json file"""
+        args = line.split()
+        actual_class = ["BaseModel"]
+
+        if len(args) <= 0:
+            print("** class name missing **")
+
+        elif args[0] not in actual_class:
+            print("** class doesn't exist**")
+
+        else:
+            target_class = globals()[args[0]]
+            new = target_class()
+            print(new.id)
+            new.save()
 
 
 if __name__ == '__main__':

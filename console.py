71756eb064ff521622adf82a98591c83a953ffd3
EsraaGD <isra.hatim2@gmail.com>
2024-01-10 08:59:54 -0800

    Console 6 up & working

--- console.py
+++ console.py
@@ -10,11 +10,11 @@ class HBNBCommand(cmd.Cmd):
     prompt = "(hbnb)"
 
     def do_quit(self, *args):
-        """Exit prog"""
+        """Quit command to exit the program"""
         return True
 
     def do_EOF(self, *args):
-        """Exit prog"""
+        """Exit program"""
         return True
 
     def emptyline(self):

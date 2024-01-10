34d35ac7ed54c310a2dd65ef58c44506365363a6
Isra Hatim <138698238+EsraaGD@users.noreply.github.com>
2024-01-11 01:36:37 +0300

    Show 7

--- console.py
+++ console.py
@@ -9,7 +9,8 @@ from models import storage
 class HBNBCommand(cmd.Cmd):
     """Entry point to CI"""
 
-    prompt = "(hbnb)"
+    prompt = "(hbnb) "
+    actual_class = ["BaseModel"]
 
     def do_quit(self, *args):
         """Quit command to exit the program"""
@@ -26,12 +27,11 @@ class HBNBCommand(cmd.Cmd):
     def do_create(self, line):
         """Create instance of BaseModel & save json file"""
         args = line.split()
-        actual_class = ["BaseModel"]
 
-        if len(args) <= 0:
+        if len(args) == 0:
             print("** class name missing **")
 
-        elif args[0] not in actual_class:
+        elif args[0] not in HBNBCommand.actual_class:
             print("** class doesn't exist**")
 
         else:
@@ -39,6 +39,27 @@ class HBNBCommand(cmd.Cmd):
             new = target_class()
             print(new.id)
             new.save()
+
+    def do_show(self, line):
+        """Prints the string representation of instance based on class name & id"""
+        args = line.split()
+
+        if len(args) == 0:
+            print("** class name missing **")
+        elif args[0] not in HBNBCommand.actual_class:
+            print("** class doesn't exist **")
+        elif len(args) <= 1:
+            print("** instance id missing **")
+        else:
+            target_class = globals()[args[0]]
+            inst_id = args[1]
+            if target_class:
+                key = f"{args[0]}.{inst_id}"
+                obj = storage.all()
+                if key in obj:
+                    print(obj[key])
+                else:
+                    print("** no instance found **")
 
 
 if __name__ == '__main__':

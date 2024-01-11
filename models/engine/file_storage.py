ddd277792e7262e3151b9846ca23855df56cf3a1
oovaa <omarvx211@gmail.com>
2024-01-11 11:04:41 +0200

    fixed reloading User obj at 11-11

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -2,7 +2,6 @@
 
 
 import json
-import os
 from models.base_model import BaseModel
 from models.user import User
 
--- models/user.py
+++ models/user.py
@@ -10,3 +10,10 @@ class User(BaseModel):
     password = ""
     first_name = ""
     last_name = ""
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(args, kwargs)
+        self.email = kwargs.get('email', "")
+        self.password = kwargs.get('password', "")
+        self.first_name = kwargs.get('first_name', "")
+        self.last_name = kwargs.get('last_name', "")

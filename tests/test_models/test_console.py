dd60826191ed8645f29ea265a3b6b35ecb6d475e
Isra Hatim <138698238+EsraaGD@users.noreply.github.com>
2024-01-13 22:22:18 +0300

    Pycodestyle

---
+++ tests/test_models/test_console.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Defines unittests for models/review.py.
+
+Unittest classes:
+    TestReview_instantiation
+    TestReview_save
+    TestReview_to_dict
+"""
+
+import unittest
+
+
+class TestReview_instantiation(unittest.TestCase):
+    pass

1be54f720fdd5e91f87faf9edfe301ff7c09c67c
oovaa <omarvx211@gmail.com>
2024-01-14 22:08:04 +0200

    test User errors at 14-22

--- tests/test_console.py
+++ tests/test_console.py
@@ -182,9 +182,9 @@ class TestConsoleWithUser(unittest.TestCase):
         user_instance = User()
         user_id = user_instance.id
         with patch('sys.stdout', new=StringIO()) as mock_stdout:
-            self.getCommandOutput(f"show User {user_id}")
-            output = mock_stdout.getvalue().strip()
-            self.assertTrue(str(user_instance) in output)
+            output = self.getCommandOutput(f"show User {user_id}")
+            expected_str = str(user_instance)
+            self.assertEqual(expected_str, output)
 
     def test_destroy_user(self):
         user_instance = User()
@@ -210,6 +210,111 @@ class TestConsoleWithUser(unittest.TestCase):
                 f"update User {user_id} email 'test@example.com'")
             output = mock_stdout.getvalue().strip()
             self.assertTrue(len(output) == 0)
+
+
+class Test_User_errors(unittest.TestCase):
+
+    def setUp(self):
+        self.hbnb_command = HBNBCommand()
+
+    def getCommandOutput(self, command):
+        with patch('sys.stdout', new=StringIO()) as mock_stdout:
+            self.hbnb_command.onecmd(command)
+            return mock_stdout.getvalue().strip()
+
+    def test_create_class_name_missing(self):
+        with patch('sys.stdout', new=StringIO()) as mock_stdout:
+            output = self.getCommandOutput("create")
+            self.assertEqual(output, "** class name missing **")
+
+    def test_create_class_not_exist(self):
+        with patch('sys.stdout', new=StringIO()) as mock_stdout:
+            output = self.getCommandOutput("create MyModel")
+            self.assertEqual(output, "** class doesn't exist **")
+
+    def test_show_class_name_missing(self):
+        with patch('sys.stdout', new=StringIO()) as mock_stdout:
+            output = self.getCommandOutput("show")
+            self.assertEqual(output, "** class name missing **")
+
+    def test_show_class_not_exist(self):
+        with patch('sys.stdout', new=StringIO()) as mock_stdout:
+            output = self.getCommandOutput("show MyModel")
+            self.assertEqual(output, "** class doesn't exist **")
+
+    def test_show_id_not_exist(self):
+        with patch('sys.stdout', new=StringIO()) as mock_stdout:
+            output = self.getCommandOutput("show User")
+            self.assertEqual(output, "** instance id missing **")
+
+    def test_show_id_not_found(self):
+        with patch('sys.stdout', new=StringIO()) as mock_stdout:
+            output = self.getCommandOutput("show User 213415ms")
+            self.assertEqual(output, "** no instance found **")
+
+    def test_destroy_class_name_missing(self):
+        with patch('sys.stdout', new=StringIO()) as mock_stdout:
+            output = self.getCommandOutput("destroy")
+            self.assertEqual(output, "** class name missing **")
+
+    def test_destroy_class_not_exist(self):
+        with patch('sys.stdout', new=StringIO()) as mock_stdout:
+            output = self.getCommandOutput("destroy MyModel")
+            self.assertEqual(output, "** class doesn't exist **")
+
+    def test_destroy_instance_id_missing(self):
+        with patch('sys.stdout', new=StringIO()) as mock_stdout:
+            output = self.getCommandOutput("destroy User")
+            self.assertEqual(output, "** instance id missing **")
+
+    def test_destroy_no_instance_found(self):
+        with patch('sys.stdout', new=StringIO()) as mock_stdout:
+            output = self.getCommandOutput("destroy User 121212")
+            self.assertEqual(output, "** no instance found **")
+
+    def test_all_with_class_name(self):
+        with patch('sys.stdout', new=StringIO()) as mock_stdout:
+            output = self.getCommandOutput("all User")
+            # You may want to assert the format of the output if it's specific
+            self.assertNotEqual(output, "** class doesn't exist **")
+
+    def test_all_nonexistent_class(self):
+        with patch('sys.stdout', new=StringIO()) as mock_stdout:
+            output = self.getCommandOutput("all MyModel")
+            self.assertEqual(output, "** class doesn't exist **")
+
+    def test_update_class_name_missing(self):
+        with patch('sys.stdout', new=StringIO()) as mock_stdout:
+            output = self.getCommandOutput("update")
+            self.assertEqual(output, "** class name missing **")
+
+    def test_update_class_not_exist(self):
+        with patch('sys.stdout', new=StringIO()) as mock_stdout:
+            output = self.getCommandOutput("update MyModel")
+            self.assertEqual(output, "** class doesn't exist **")
+
+    def test_update_instance_id_missing(self):
+        with patch('sys.stdout', new=StringIO()) as mock_stdout:
+            output = self.getCommandOutput("update User")
+            self.assertEqual(output, "** instance id missing **")
+
+    def test_update_no_instance_found(self):
+        with patch('sys.stdout', new=StringIO()) as mock_stdout:
+            output = self.getCommandOutput("update User 121212")
+            self.assertEqual(output, "** no instance found **")
+
+    def test_update_attribute_name_missing(self):
+        to_update = User()
+        with patch('sys.stdout', new=StringIO()) as mock_stdout:
+            output = self.getCommandOutput(f"update User {to_update.id}")
+            self.assertEqual(output, "** attribute name missing **")
+
+    def test_update_value_missing(self):
+        to_update = User()
+        with patch('sys.stdout', new=StringIO()) as mock_stdout:
+            output = self.getCommandOutput(
+                f"update User {to_update.id} first_name")
+            self.assertEqual(output, "** value missing **")
 
 
 if __name__ == '__main__':
